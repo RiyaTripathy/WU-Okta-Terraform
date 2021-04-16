d9308f114293008ee41bf6217a2e9d6d944b2395
RiyaTripathy <riya.tripathy9127@gmail.com>
2021-04-16 19:57:02 +0530

    Added user attribute

--- identity.tf
+++ identity.tf
@@ -7,41 +7,9 @@ provider "okta" {
     base_url = var.base_url
     api_token = var.api_token
 }
-resource "okta_group" "FBImember_group" {
-  name        = "FBI Member"
-  description = "FBI Member with role code EXT1"
-}
-
-resource "okta_group" "FBImemberadmin_group" {
-  name        = "FBI Member Admin"
-  description = "FBI Member Admin with role code EXT2"
-}
-data "okta_auth_server" "authserver_test" {
-  name = "default"
-}
-resource "okta_auth_server_claim" "div_claim" {
-  auth_server_id = data.okta_auth_server.authserver_test.id
-  name           = "division_code"
-  value_type     = "EXPRESSION"
-  value          = "user.divisions"
-  claim_type     = "RESOURCE"
-}
-resource "okta_auth_server_claim" "role_claim" {
-  auth_server_id = data.okta_auth_server.authserver_test.id
-  name           = "role"
-  value_type     = "EXPRESSION"
-  value          = "user.role_code"
-  claim_type     = "RESOURCE"
-}
-resource "okta_auth_server_claim" "company_claim" {
-  auth_server_id = data.okta_auth_server.authserver_test.id
-  name           = "company_code"
-  value_type     = "EXPRESSION"
-  value          = "user.company"
-  claim_type     = "RESOURCE"
-}
-resource "okta_trusted_origin" "trusted_test" {
-  name   = "FBI Benchmark URL"
-  origin = "https://benchmark.performanceroundtables.com/"
-  scopes = ["CORS","REDIRECT"]
+resource "okta_user_schema" "dob_extension" {
+  index  = "date_of_birth"
+  title  = "Date of Birth"
+  type   = "string"
+  master = "PROFILE_MASTER"
 }
